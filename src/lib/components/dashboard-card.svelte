35b67a4f8d7c5038fe685986ea37187ca1265d04
Jekael front <224990958+jklayam25-sudo@users.noreply.github.com>
2025-10-29 20:08:21 +0700

    Dashboard Page 80%

---
+++ src/lib/components/dashboard-card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Helper } from "flowbite-svelte";
+
+	let props = $props();
+</script>
+
+<div class="{props.background} rounded-xl flex p-2 gap-2 group cursor-pointer m-1">
+      <div class="{props.iconBg} rounded-xl p-1">
+        <svelte:component this={props.icon} class="w-10 h-10" />
+      </div>
+      <div >
+        <span class="{props.spanClass}">{props.data}</span>
+        <Helper class="text-gray-100">{props.text}</Helper>
+      </div>
+</div>
